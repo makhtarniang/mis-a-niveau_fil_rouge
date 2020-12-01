18b388aa780dace400861b9909618e7be91dd3c9
Niang Makhou <niangmakhtar4@gmail.com>
2020-12-01 15:57:15 +0000

    4em commit

--- mis_a_niveau_fil_rouge/src/Entity/Apprenant.php
+++ mis_a_niveau_fil_rouge/src/Entity/Apprenant.php
@@ -2,6 +2,8 @@
 
 namespace App\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use App\Repository\ApprenantRepository;
 use ApiPlatform\Core\Annotation\ApiResource;
@@ -19,8 +21,71 @@ class Apprenant extends User
      */
     protected $id;
 
+    /**
+     * @ORM\ManyToMany(targetEntity=Groupe::class, mappedBy="apprenants")
+     */
+    private $Groupe;
+
+
+    public function __construct()
+    {
+        $this->Groupe = new ArrayCollection();
+        $this->apprenants = new ArrayCollection();
+    }
+
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    /**
+     * @return Collection|self[]
+     */
+   /* public function getGroupe(): Collection
+    {
+        return $this->Groupe;
+    }
+
+    public function addGroupe(self $groupe): self
+    {
+        if (!$this->Groupe->contains($groupe)) {
+            $this->Groupe[] = $groupe;
+        }
+
+        return $this;
+    }
+
+    public function removeGroupe(self $groupe): self
+    {
+        $this->Groupe->removeElement($groupe);
+
+        return $this;
+    }
+*/
+    /**
+     * @return Collection|self[]
+     */
+    public function getApprenants(): Collection
+    {
+        return $this->apprenants;
+    }
+
+    public function addApprenant(self $apprenant): self
+    {
+        if (!$this->apprenants->contains($apprenant)) {
+            $this->apprenants[] = $apprenant;
+            $apprenant->addGroupe($this);
+        }
+
+        return $this;
+    }
+
+    public function removeApprenant(self $apprenant): self
+    {
+        if ($this->apprenants->removeElement($apprenant)) {
+            $apprenant->removeGroupe($this);
+        }
+
+        return $this;
     }
 }
--- mis_a_niveau_fil_rouge/src/Entity/Formateur.php
+++ mis_a_niveau_fil_rouge/src/Entity/Formateur.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Entity\User;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use App\Repository\FormateurRepository;
 use ApiPlatform\Core\Annotation\ApiResource;
@@ -19,8 +21,42 @@ class Formateur extends User
      */
     protected $id;
 
+    /**
+     * @ORM\ManyToMany(targetEntity=Promo::class, inversedBy="formateurs")
+     */
+    private $Promo;
+
+    public function __construct()
+    {
+        $this->Promo = new ArrayCollection();
+    }
+
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    /**
+     * @return Collection|Promo[]
+     */
+    public function getPromo(): Collection
+    {
+        return $this->Promo;
+    }
+
+    public function addPromo(Promo $promo): self
+    {
+        if (!$this->Promo->contains($promo)) {
+            $this->Promo[] = $promo;
+        }
+
+        return $this;
+    }
+
+    public function removePromo(Promo $promo): self
+    {
+        $this->Promo->removeElement($promo);
+
+        return $this;
     }
 }
